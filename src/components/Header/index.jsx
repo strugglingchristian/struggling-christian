5707c090f13655a7eea3b3ce38e5c7589a6c9892
strugglingchristian <christian.i.septimo@gmail.com>
2022-06-27 19:34:15 +0800

    Fix responsive navigation

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,13 +1,14 @@
-import { Container, Row, Col } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Container, Row, Col, Navbar, Nav } from "react-bootstrap";
 import "./style.css";
 
 function Header() {
   return (
     <Container>
       <Row className="header">
-        <Col md={1}>&nbsp;</Col>
-        <Col md={10}>
+        <Col sm={1} md={2}>
+          &nbsp;
+        </Col>
+        <Col sm={9} md={8}>
           <Row>
             <Row className="header-title">
               <Col className="d-flex">Struggling Christian</Col>
@@ -18,24 +19,27 @@ function Header() {
               </Col>
             </Row>
             <Row>
-              <div className="nav-links-container">
-                <Link className="nav-link" to="/">
-                  Home
-                </Link>
-                <Link className="nav-link" to="/cultic-doctrines">
-                  Cultic Doctrines
-                </Link>
-                <Link className="nav-link" to="/articles">
-                  Articles
-                </Link>
-                <Link className="nav-link" to="/about">
-                  About
-                </Link>
-              </div>
+              <Navbar collapseOnSelect expand="lg" variant="dark" bg="dark">
+                <Container fluid className="px-0">
+                  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                  <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto">
+                      <Nav.Link href="/">Home</Nav.Link>
+                      <Nav.Link href="/cultic-doctrines">
+                        Dubious Doctrines
+                      </Nav.Link>
+                      <Nav.Link href="/articles">Articles</Nav.Link>
+                      <Nav.Link href="/about">About</Nav.Link>
+                    </Nav>
+                  </Navbar.Collapse>
+                </Container>
+              </Navbar>
             </Row>
           </Row>
         </Col>
-        <Col md={1}>&nbsp;</Col>
+        <Col sm={1} md={1}>
+          &nbsp;
+        </Col>
       </Row>
     </Container>
   );
