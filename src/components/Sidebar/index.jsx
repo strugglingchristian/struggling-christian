fae74e9bfef604fe818d358f6ee324b83dc116f6
strugglingchristian <christian.i.septimo@gmail.com>
2022-06-29 20:09:19 +0800

    Move affiliate links images inside columns

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,4 +1,4 @@
-import { Container, Row } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 
 import "./style.css";
 
@@ -114,47 +114,49 @@ function AmazonAffiliateLinkSidebar() {
   return (
     <div className="sidebar">
       <div className="sidebar-header">RECOMMENDED BOOKS</div>
-      <Row className="pb-3">
-        <div className="affiliate-container">
-          <iframe
-            title="The White Lie"
-            sandbox="allow-popups allow-scripts allow-modals allow-forms allow-same-origin"
-            style={{ width: "120px", height: "240px" }}
-            marginWidth="0"
-            marginHeight="0"
-            scrolling="no"
-            frameBorder="0"
-            src="//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ss&ref=as_ss_li_til&ad_type=product_link&tracking_id=clowreed-20&language=en_US&marketplace=amazon&region=US&placement=0960742417&asins=0960742417&linkId=e02dc5e7e64772b8891c06332dbc6d3a&show_border=true&link_opens_in_new_window=true"
-          />
-        </div>
-      </Row>
-      <Row className="pb-3">
-        <div className="affiliate-container">
-          <iframe
-            title="Sabbath in Christ"
-            sandbox="allow-popups allow-scripts allow-modals allow-forms allow-same-origin"
-            style={{ width: "120px", height: "240px" }}
-            marginWidth="0"
-            marginHeight="0"
-            scrolling="no"
-            frameBorder="0"
-            src="//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ss&ref=as_ss_li_til&ad_type=product_link&tracking_id=clowreed-20&language=en_US&marketplace=amazon&region=US&placement=0974767980&asins=0974767980&linkId=ba98f57d7a3bad3478e49c085914e54f&show_border=true&link_opens_in_new_window=true"
-          />
-        </div>
-      </Row>
-      <Row className="pb-0">
-        <div className="affiliate-container">
-          <iframe
-            title="Decoding Nicea"
-            sandbox="allow-popups allow-scripts allow-modals allow-forms allow-same-origin"
-            style={{ width: "120px", height: "240px" }}
-            marginWidth="0"
-            marginHeight="0"
-            scrolling="no"
-            frameBorder="0"
-            src="//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ss&ref=as_ss_li_til&ad_type=product_link&tracking_id=clowreed-20&language=en_US&marketplace=amazon&region=US&placement=B00MPXOV9G&asins=B00MPXOV9G&linkId=e9551ee9d5ba071ed2011413770947c5&show_border=true&link_opens_in_new_window=true"
-          />
-        </div>
+      <Row>
+        <Col className="pb-3">
+          <div className="affiliate-container">
+            <iframe
+              title="The White Lie"
+              sandbox="allow-popups allow-scripts allow-modals allow-forms allow-same-origin"
+              style={{ width: "120px", height: "240px" }}
+              marginWidth="0"
+              marginHeight="0"
+              scrolling="no"
+              frameBorder="0"
+              src="//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ss&ref=as_ss_li_til&ad_type=product_link&tracking_id=clowreed-20&language=en_US&marketplace=amazon&region=US&placement=0960742417&asins=0960742417&linkId=e02dc5e7e64772b8891c06332dbc6d3a&show_border=true&link_opens_in_new_window=true"
+            />
+          </div>
+        </Col>
+        <Col className="pb-3">
+          <div className="affiliate-container">
+            <iframe
+              title="Sabbath in Christ"
+              sandbox="allow-popups allow-scripts allow-modals allow-forms allow-same-origin"
+              style={{ width: "120px", height: "240px" }}
+              marginWidth="0"
+              marginHeight="0"
+              scrolling="no"
+              frameBorder="0"
+              src="//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ss&ref=as_ss_li_til&ad_type=product_link&tracking_id=clowreed-20&language=en_US&marketplace=amazon&region=US&placement=0974767980&asins=0974767980&linkId=ba98f57d7a3bad3478e49c085914e54f&show_border=true&link_opens_in_new_window=true"
+            />
+          </div>
+        </Col>
+        <Col className="pb-3">
+          <div className="affiliate-container">
+            <iframe
+              title="Decoding Nicea"
+              sandbox="allow-popups allow-scripts allow-modals allow-forms allow-same-origin"
+              style={{ width: "120px", height: "240px" }}
+              marginWidth="0"
+              marginHeight="0"
+              scrolling="no"
+              frameBorder="0"
+              src="//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ss&ref=as_ss_li_til&ad_type=product_link&tracking_id=clowreed-20&language=en_US&marketplace=amazon&region=US&placement=B00MPXOV9G&asins=B00MPXOV9G&linkId=e9551ee9d5ba071ed2011413770947c5&show_border=true&link_opens_in_new_window=true"
+            />
+          </div>
+        </Col>
       </Row>
     </div>
   );
