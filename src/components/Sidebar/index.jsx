b2fd0114462e09b44b4db7b8d29c5670414e2c9e
strugglingchristian <christian.i.septimo@gmail.com>
2022-06-29 17:44:04 +0800

    Add Amazon affiliate links

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -46,7 +46,7 @@ function BibleStudyResourcesSidebar() {
 function DubiousDoctrinesSidebar() {
   return (
     <div className="sidebar">
-      <div className="sidebar-header">CULT OR NOT?</div>
+      <div className="sidebar-header">TRUTH OR CULT?</div>
       <div className="sidebar-title">Jehovah&apos; Witnesses</div>
       <div className="sidebar-title">Seventh-day Adventists</div>
       <div className="sidebar-title">Latter-day Saints</div>
@@ -59,7 +59,7 @@ function DubiousDoctrinesSidebar() {
 function YoutubeChannelsSidebar() {
   return (
     <div className="sidebar">
-      <div className="sidebar-header">RECOMMEND CHANNELS</div>
+      <div className="sidebar-header">RECOMMENDED CHANNELS</div>
       <div className="sidebar-title">
         <a
           target="_blank"
@@ -110,6 +110,56 @@ function YoutubeChannelsSidebar() {
   );
 }
 
+function AmazonAffiliateLinkSidebar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebar-header">RECOMMENDED BOOKS</div>
+      <Row className="pb-3">
+        <div className="affiliate-container">
+          <iframe
+            title="The White Lie"
+            sandbox="allow-popups allow-scripts allow-modals allow-forms allow-same-origin"
+            style={{ width: "120px", height: "240px" }}
+            marginWidth="0"
+            marginHeight="0"
+            scrolling="no"
+            frameBorder="0"
+            src="//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ss&ref=as_ss_li_til&ad_type=product_link&tracking_id=clowreed-20&language=en_US&marketplace=amazon&region=US&placement=0960742417&asins=0960742417&linkId=e02dc5e7e64772b8891c06332dbc6d3a&show_border=true&link_opens_in_new_window=true"
+          />
+        </div>
+      </Row>
+      <Row className="pb-3">
+        <div className="affiliate-container">
+          <iframe
+            title="Sabbath in Christ"
+            sandbox="allow-popups allow-scripts allow-modals allow-forms allow-same-origin"
+            style={{ width: "120px", height: "240px" }}
+            marginWidth="0"
+            marginHeight="0"
+            scrolling="no"
+            frameBorder="0"
+            src="//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ss&ref=as_ss_li_til&ad_type=product_link&tracking_id=clowreed-20&language=en_US&marketplace=amazon&region=US&placement=0974767980&asins=0974767980&linkId=ba98f57d7a3bad3478e49c085914e54f&show_border=true&link_opens_in_new_window=true"
+          />
+        </div>
+      </Row>
+      <Row className="pb-0">
+        <div className="affiliate-container">
+          <iframe
+            title="Decoding Nicea"
+            sandbox="allow-popups allow-scripts allow-modals allow-forms allow-same-origin"
+            style={{ width: "120px", height: "240px" }}
+            marginWidth="0"
+            marginHeight="0"
+            scrolling="no"
+            frameBorder="0"
+            src="//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ss&ref=as_ss_li_til&ad_type=product_link&tracking_id=clowreed-20&language=en_US&marketplace=amazon&region=US&placement=B00MPXOV9G&asins=B00MPXOV9G&linkId=e9551ee9d5ba071ed2011413770947c5&show_border=true&link_opens_in_new_window=true"
+          />
+        </div>
+      </Row>
+    </div>
+  );
+}
+
 function Sidebar() {
   return (
     <Container>
@@ -118,6 +168,9 @@ function Sidebar() {
       </Row>
       <Row className="sidebar-row-group">
         <YoutubeChannelsSidebar />
+      </Row>
+      <Row className="sidebar-row-group">
+        <AmazonAffiliateLinkSidebar />
       </Row>
       <Row className="sidebar-row-group">
         <DubiousDoctrinesSidebar />
