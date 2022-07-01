cb9cfd9f4c54b02fed5bc1bfd25d30ea8199151d
strugglingchristian <christian.i.septimo@gmail.com>
2022-07-01 19:39:39 +0800

    Added Raleway custom font

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import "./fonts/Raleway/Raleway-Bold.ttf";
+import "./fonts/Raleway/Raleway-Italic.ttf";
+import "./fonts/Raleway/Raleway-Light.ttf";
+import "./fonts/Raleway/Raleway-Medium.ttf";
+import "./fonts/Raleway/Raleway-Regular.ttf";
+import "./fonts/Raleway/Raleway-SemiBold.ttf";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
