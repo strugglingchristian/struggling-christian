ba022ba2bccdfd5e67e9704a77ef20509e2778e8
strugglingchristian <christian.i.septimo@gmail.com>
2022-11-29 22:23:21 +0800

    Add scrolling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import MainRoute from "./routes/main-route";
+import ScrollToTop from "./components/ScrollToTop";
 import "./styles/App.css";
 
 function App() {
   return (
     <div className="App">
+      <ScrollToTop />
       <MainRoute />
     </div>
   );
