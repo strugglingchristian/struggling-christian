f49a46e5a992f48e5ca09de5d0f3c88cb48b9bcd
strugglingchristian <christian.i.septimo@gmail.com>
2022-11-30 23:10:06 +0800

    Initial implementation of custom breadcrumbs

---
+++ src/components/BreadCrumb.jsx
@@ -0,0 +1,116 @@
+/* eslint-disable */
+import { Link, useLocation } from "react-router-dom";
+import "../styles/breadcrumb.css";
+
+const getBreadcrumbPath = (location) => {
+  const crumbs = [{ title: "Home", url: "/" }];
+  let title = "";
+  if (location.pathname === "/") {
+    title = "Home";
+  } else if (location.pathname.startsWith("/dubious-doctrines")) {
+    title = "Dubius Doctrines";
+    crumbs.push({ title, url: "/dubious-doctrines" });
+    if (location.pathname.startsWith("/dubious-doctrines/lds")) {
+      title = "LDS";
+      crumbs.push({ title, url: "/dubious-doctrines/lds" });
+      if (location.pathname.startsWith("/dubious-doctrines/lds/coming-soon")) {
+        crumbs.push({
+          title: "Coming Soon",
+          url: location.pathname
+        });
+      }
+    } else if (location.pathname.startsWith("/dubious-doctrines/sda")) {
+      title = "SDA";
+      crumbs.push({ title, url: "/dubious-doctrines/sda" });
+      if (
+        location.pathname.startsWith("/dubious-doctrines/sda/authority-of-egw")
+      ) {
+        crumbs.push({
+          title: "The Authority of Ellen G. White as a prophet",
+          url: location.pathname
+        });
+      } else if (
+        location.pathname.startsWith("/dubious-doctrines/sda/coming-soon")
+      ) {
+        crumbs.push({
+          title: "Coming Soon",
+          url: location.pathname
+        });
+      }
+    } else if (location.pathname.startsWith("/dubious-doctrines/jw")) {
+      title = "JW";
+      crumbs.push({ title, url: "/dubious-doctrines/jw" });
+      if (location.pathname.startsWith("/dubious-doctrines/jw/coming-soon")) {
+        crumbs.push({
+          title: "Coming Soon",
+          url: location.pathname
+        });
+      }
+    } else if (location.pathname.startsWith("/dubious-doctrines/inc")) {
+      title = "INC";
+      crumbs.push({ title, url: "/dubious-doctrines/inc" });
+      if (location.pathname.startsWith("/dubious-doctrines/inc/coming-soon")) {
+        crumbs.push({
+          title: "Coming Soon",
+          url: location.pathname
+        });
+      }
+    }
+  } else if (location.pathname === "/about") {
+    title = "About";
+    crumbs.push({ title, url: location.pathname });
+  } else if (location.pathname.startsWith("/articles")) {
+    title = "Articles";
+    crumbs.push({ title, url: "/articles" });
+    if (
+      location.pathname.startsWith("/articles/1844-made-complicated-part-1")
+    ) {
+      crumbs.push({
+        title: "1844 Made Complicated (pt. 1)",
+        url: location.pathname
+      });
+    } else if (
+      location.pathname.startsWith("/articles/1844-made-complicated-part-2")
+    ) {
+      crumbs.push({
+        title: "1844 Made Complicated (pt. 2)",
+        url: location.pathname
+      });
+    } else if (location.pathname.startsWith("/articles/egw-true-prophet")) {
+      crumbs.push({
+        title: "Is Ellen G. White a True Prophet?",
+        url: location.pathname
+      });
+    } else if (location.pathname.startsWith("/articles/coming-soon")) {
+      crumbs.push({
+        title: "Coming Soon",
+        url: location.pathname
+      });
+    }
+  }
+
+  return crumbs;
+};
+
+function BreadCrumb() {
+  const location = useLocation();
+  const crumbs = getBreadcrumbPath(location);
+
+  if (crumbs.length > 1) {
+    return (
+      <div id="breadcrumb">
+        <ul>
+          {crumbs.map((crumb) => {
+            return (
+              <li>
+                <Link to={crumb.url}>{crumb.title} /</Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default BreadCrumb;
--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -1,16 +1,36 @@
 import { Container, Row, Col } from "react-bootstrap";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Header from "../Header";
 import Sidebar from "../Sidebar";
 import Footer from "../Footer";
+import BreadCrumb from "../BreadCrumb";
 import "../../styles/layout.css";
 
 function Layout() {
+  const location = useLocation();
   return (
     <Container fluid>
       <Row className="header-row">
         <Header />
       </Row>
+      {location.pathname !== "/" && (
+        <Row className="breadcrumb-row">
+          <Container fluid>
+            <Row className="pt-2 pt-lg-3 pb-0">
+              <Col xl={2} xs={12} className="d-none d-xl-block">
+                &nbsp;
+              </Col>
+              <Col xl={6} xs={12} className="px-0 mx-0">
+                <BreadCrumb />
+              </Col>
+              <Col xl={4} xs={12}>
+                &nbsp;
+              </Col>
+            </Row>
+          </Container>
+        </Row>
+      )}
+
       <Row className="content-row">
         <Container fluid>
           <Row className="pb-3">
