59f6f6d213d25f2c3c1c5b370ff2eda473c9a010
strugglingchristian <christian.i.septimo@gmail.com>
2022-11-30 20:33:57 +0800

    Style the 404 page

--- src/pages/404/index.jsx
+++ src/pages/404/index.jsx
@@ -1,7 +1,26 @@
+import Button from "react-bootstrap/Button";
+import "../../styles/lost-404.css";
+
 function Page404() {
   return (
-    <div>
-      <h1>Page not found </h1>
+    <div className="lost-404">
+      <section>
+        <div className="text-content">
+          <h1 className="lost-title">Feeling lost? </h1>
+          <p>This site doesn&apos;t have all the answers but Jesus does.</p>
+          <p>
+            Jesus said, “I am the way and the truth and the life. No one comes
+            to the Father except through me. <sub>John 14:16</sub>
+          </p>
+
+          <p>But we can redirect you back to the home page</p>
+          <div className="back-to-home">
+            <Button variant="success" href="/">
+              Back to Home
+            </Button>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
