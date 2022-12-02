d1cfce0c08bd15849758955308345c9519c1f4a0
strugglingchristian <christian.i.septimo@gmail.com>
2022-12-02 21:16:27 +0800

    Style coming-soon page

--- src/pages/coming-soon/index.jsx
+++ src/pages/coming-soon/index.jsx
@@ -1,20 +1,23 @@
+import Button from "react-bootstrap/Button";
+import "../../styles/coming-soon.css";
+
 function ComingSoon() {
   return (
-    <div id="coming-soon">
-      <div className="body-content">
-        <h1 className="title">Coming Soon</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin posuere
-          enim eu accumsan pulvinar. Mauris sagittis vestibulum nisl, molestie
-          fringilla velit eleifend id. Duis a metus dictum, imperdiet tellus
-          vel, auctor nisl. Phasellus et magna blandit, tincidunt lectus ut,
-          vehicula nisi. Suspendisse a sodales massa, convallis semper odio.
-          Praesent vitae diam purus. Vestibulum convallis in turpis elementum
-          fermentum. Sed eget risus ullamcorper, dignissim ipsum at, blandit
-          neque. Donec rhoncus euismod nisl in faucibus. Integer sit amet ligula
-          erat. Quisque ac nisl in nibh tincidunt pulvinar.
-        </p>
-      </div>
+    <div className="coming-soon">
+      <section>
+        <div className="text-content">
+          <h1 className="coming-soon-title">Under Construction</h1>
+          <p>
+            This site is still under construction. Please check back again later
+          </p>
+
+          <div className="back-to-home">
+            <Button variant="success" href="/">
+              Back to Home
+            </Button>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
