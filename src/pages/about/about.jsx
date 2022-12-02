0e64d7ed35526f66ba25ab97b89946ed9980f154
strugglingchristian <christian.i.septimo@gmail.com>
2022-12-02 21:55:35 +0800

    Draft the about page

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -1,18 +1,27 @@
+/* eslint-disable */
 function About() {
   return (
     <div id="about">
       <div className="body-content">
         <h1 className="title">About</h1>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin posuere
-          enim eu accumsan pulvinar. Mauris sagittis vestibulum nisl, molestie
-          fringilla velit eleifend id. Duis a metus dictum, imperdiet tellus
-          vel, auctor nisl. Phasellus et magna blandit, tincidunt lectus ut,
-          vehicula nisi. Suspendisse a sodales massa, convallis semper odio.
-          Praesent vitae diam purus. Vestibulum convallis in turpis elementum
-          fermentum. Sed eget risus ullamcorper, dignissim ipsum at, blandit
-          neque. Donec rhoncus euismod nisl in faucibus. Integer sit amet ligula
-          erat. Quisque ac nisl in nibh tincidunt pulvinar.
+          Struggling is hard. Struggling is not fun. Struggling is inconvenient.
+          Why struggle when you can go with the flow? In this day and age, it's
+          easy to just give up, be your natural self and be one with the world.
+          But as Christians, we were called by God to a higher standard. We are
+          in this world, but we are not of the world.
+        </p>
+        <p>
+          We are called by God to love the people of the world, but we are not
+          to love the world and it's false belief systems that are in conflict
+          with God. It would be easier to be politically correct all the time
+          and not step on anybody's toes and hope we can all get along and love
+          each other and live in peace but the reality is this is extremely
+          hard, if not impossible.
+        </p>
+        <p>
+          After all, Jesus himself said that He did not come to bring peace but
+          a sword.
         </p>
       </div>
     </div>
