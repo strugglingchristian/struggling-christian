1440cc6cdcb2b4851b92cf8dc7f581dcac96b20d
strugglingchristian <christian.i.septimo@gmail.com>
2022-12-17 23:53:37 +0800

    Finish about content

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -42,7 +42,8 @@ function About() {
               good, but I cannot carry it out. <sup>19</sup>For I do not do the
               good I want to do, but the evil I do not want to do—this I keep on
               doing. <sup>20</sup>Now if I do what I do not want to do, it is no
-              longer I who do it, but it is sin living in me that does it.
+              longer I who do it, but it is sin living in me that does it.{" "}
+              <br />
               Romans 7:15-20
             </em>
           </p>
@@ -51,56 +52,165 @@ function About() {
         <p>
           <ol>
             <li>
-              If you’re a Christian who believes in eternal security, you’re
-              probably thinking “You shouldn’t trouble yourself about this
-              because there is nothing on earth or heaven that can take away
-              your salvation.
+              If you’re a Jew, just don’t violate any of the 613 commandments of
+              the Torah and if you have committed some sin, you can repent,
+              fast, give to charity, and do some other <em>mitzvot</em> and
+              you’ll be fine.
             </li>
             <li>
-              Or If you’re a Christian who believes in conditional security, you
+              If you’re a Muslim, avoid doing the major and minor sins, repent
+              and observe the <em>Five Pillars</em> of Islam.
+            </li>
+            <li>
+              If you’re Catholic, try not to do any mortal sins as they are a
+              sure ticket to damnation. But if you do sin, just confess to a
+              priest, do some penance and in the unfortunate event that you died
+              and have found yourself in purgatory instead of heaven, your
+              living loved ones could give indulgence to help you on your way to
+              heaven. Too bad if you’re in hell, though, you’re gonna stay there
+              forever.
+            </li>
+            <li>
+              If you’re a Christian who believes in eternal security, you might
+              say, “You shouldn’t trouble yourself about this because there is
+              nothing on earth or heaven that can take away your salvation.
+            </li>
+            <li>
+              If you’re a Christian who believes in conditional security, you
               might say “why worry about sinning when you have already been
               saved by grace through faith and that the Holy Spirit is at work
               in you so you would produce the fruit of the Spirit”
             </li>
             <li>
-              Or if you’re an atheist, you might probably be thinking, “What
-              sin? As long as you obey the laws of your country, treat others
-              with respect, and be mindful of another’s preferred pronouns -
-              basically don’t be a jerk, that’s it.{" "}
+              If you’re an atheist, you might say, “What sin? All you need to do
+              is obey the laws of the land, treat others with respect, be
+              sensitive to other people’s culture, be mindful of another’s
+              preferred pronouns, etc, etc - basically just don’t be a jerk,
+              that’s it.
             </li>
             <li>
-              <em>
-                You’re probably thinking about something else that I don’t know
-                about but it’s ok because it’s not one of my points.{" "}
-              </em>
+              If you’re a Hindu or Buddhist, doing bad deeds would keep you
+              trapped in this constant cycle of deaths and rebirths so you
+              should keep on racking up those good points by performing good
+              deeds to cancel out your bad karma
+            </li>
+            <li>
+              Or you’re probably thinking about something else that I haven’t
+              mentioned but that is beside the point.
             </li>
           </ol>
         </p>
+        <p>But I’ll just focus on Christianity for now.</p>
         <p>
           Why struggle as a Christian? Struggling is hard. Struggling is not
           fun. Struggling is inconvenient. In this day and age where convenience
           is expected, it would be easier to not struggle. It would be easier if
           you could just be your natural self and conform to the world's
-          standards and be one with the world.
+          standards and be one with the world. As the saying goes,{" "}
+          <em>“If you can’t beat them, join them.”</em>
+        </p>
+        <p>Most of the time, I conform.</p>
+        <p>&emsp;&emsp;Then feel guilty.</p>
+        <p>&emsp;&emsp;&emsp;&emsp;Then repent.</p>
+        <p>
+          &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;Then try to do good on my own for
+          a while…
         </p>
         <p>
-          Struggling is hard. Struggling is not fun. Struggling is inconvenient.
-          Why struggle when you can go with the flow? In this day and age, it's
-          easy to just give up, be your natural self and be one with the world.
-          But as Christians, we were called by God to a higher standard. We are
-          in this world, but we are not of the world.
+          &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;Then back to sin
+          again.
+        </p>
+        <p>Later. Rinse. Repeat.</p>
+        <p>
+          After a while, this gets tiresome and it kind of makes me wonder
+          whether I’ve been truly saved or not. I would like to believe I have
+          the assurance of salvation. After all, the apostle Paul says in
+          Ephesians that
+        </p>
+        <blockquote>
+          <p align="center">
+            <em>
+              For it is by grace you have been saved, through faith— and this is
+              not from yourselves, it is the gift of God— not by works, so that
+              no one can boast. <br />
+              Ephesians 2:8-9
+            </em>
+          </p>
+        </blockquote>
+        <p>But why don’t I feel saved?</p>
+        <p>
+          That’s the million dollar question. But I also I know for a fact that
+          my feelings are unreliable but it can certainly affect every aspect of
+          my life. It sets off a train of thought in my mind like this — If I
+          don’t feel saved maybe I wasn’t even saved in the first place, or
+          perhaps I was saved and then at some point, I have grieved the Holy
+          Spirit and He left me so now I’m on my own. Thoughts like this keep me
+          defeated and bound to sin. Until I realized that I was trying to do
+          the work on my own. When I read what Paul said in Philippians:
+        </p>
+        <blockquote>
+          <p align="center">
+            <em>
+              being confident of this, that he who began a good work in you will
+              carry it on to completion until the day of Christ Jesus. <br />
+              Philippians 1:6
+            </em>
+          </p>
+        </blockquote>
+        <p>
+          I was doing it the wrong way all along. I’ve been saved indeed by
+          grace through faith and not by works. Yet here am I doing all the work
+          when it is God who began a good work and will carry it on to
+          completion.
         </p>
         <p>
-          We are called by God to love the people of the world, but we are not
-          to love the world and it's false belief systems that are in conflict
-          with God. It would be easier to be politically correct all the time
-          and not step on anybody's toes and hope we can all get along and love
-          each other and live in peace but the reality is this is extremely
-          hard, if not impossible.
+          I think my problem was I was conflating salvation with sinlessness and
+          perfection. Sinless perfection is not achievable from this side of
+          eternity.
+        </p>
+        <blockquote>
+          <p align="center">
+            <em>
+              If we claim to be without sin, we deceive ourselves and the truth
+              is not in us. <br />1 John 1:8
+            </em>
+          </p>
+        </blockquote>
+        <p>
+          But the fact that you and I are struggling against sin means that the
+          Holy Spirit is at work in us; Changing our hearts and minds and making
+          us aware of our sins and our constant need for a Savior; Reminding us
+          that we are to take our crosses daily and follow Jesus. Sure we may
+          stumble along the way and feel like such a loser, but we must get up
+          and move forward. It says in 1 John 1:9
+        </p>
+        <blockquote>
+          <p align="center">
+            <em>
+              If we confess our sins, he is faithful and just and will forgive
+              us our sins and purify us from all unrighteousness. <br />1 John
+              1:9
+            </em>
+          </p>
+        </blockquote>
+        <p>And the writer of Hebrews urges us to struggle against sin.</p>
+        <blockquote>
+          <p align="center">
+            <em>
+              In your struggle against sin, you have not yet resisted to the
+              point of shedding your blood.
+              <br />
+              Hebrews 1:4
+            </em>
+          </p>
+        </blockquote>
+        <p>
+          So whatever your situation may be, don't be discourged and keep on
+          struggling. You may be a work in progress but know that God is at work
+          in you and will complete it until the day of Christ.
         </p>
         <p>
-          After all, Jesus himself said that He did not come to bring peace but
-          a sword.
+          <br />
         </p>
       </div>
     </div>
